4ad399bfd0b31029f26fef7bfd4837a9c9130475
Mohammad Amin Moeini <MohammadAminMoeini@outlook.com>
2022-06-01 21:09:13 +0430

    add numbers

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,6 +7,14 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+
+            int a = 12;
+            int b = 13;
+
+            int c = a + b;
+
         }
     }
 }
